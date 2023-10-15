6b017a52ac1a96ef4d7718e0435c5cdd42a9b309
EsraaSamyZ <142722769+EsraaSamyZ@users.noreply.github.com>
2023-10-15 17:17:36 +0300

    added card comp & API service

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/themovieAPI.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  movies!: any[];
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit(): void {
+    this.movieService.getPopularMovies().subscribe(data => {
+      this.movies = data.results;
+    });
+  }
 
 }
---
+++ src/services/themovieAPI.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  private apiKey = '89331dd238f7d3c8400eda6517901145';
+  private apiUrl = 'https://api.themoviedb.org/3';
+
+  constructor(private http: HttpClient) { }
+
+  getPopularMovies(): Observable<any> {
+    const url = `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`;
+    return this.http.get(url);
+  }
+}
