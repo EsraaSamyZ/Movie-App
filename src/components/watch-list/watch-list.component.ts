73db49cb0726b9f59198dd5040e4014b8a8ce696
Nourhan Ashraf <nourhanaashraf16@gmail.com>
2023-10-16 13:30:10 +0300

    get matchList movies

--- src/components/watch-list/watch-list.component.ts
+++ src/components/watch-list/watch-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WatchListService } from 'src/services/watch-list.service';
 
 @Component({
   selector: 'app-watch-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./watch-list.component.css']
 })
 export class WatchListComponent {
+favMovies : any=[] ;
+constructor(private WatchListService:WatchListService){
 
+}
+ngOnInit(){
+  this.WatchListService.getFavouritMovies().subscribe(res => {
+    this.favMovies = res;
+    console.log(this.favMovies)
+  })
+}
 }
