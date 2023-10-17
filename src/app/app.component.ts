3415aa76b4b0996b37f70d88a84305edab5bb5b6
Nourhan Ashraf <nourhanaashraf16@gmail.com>
2023-10-18 00:16:06 +0300

    editNotFound

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,16 +6,7 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class AppComponent {
   title = 'Movie-app';
-  constructor(private router: Router) {}
 
-  isHeaderVisible() {
-    // Get the current route
-    const currentRoute = this.router.url;
 
-    // Define the routes where the header should be visible
-    const visibleRoutes = ['/','/movies/:id' ,'/favmovies' ];
 
-    // Check if the current route is in the visibleRoutes array
-    return visibleRoutes.includes(currentRoute);
-  }
 }
