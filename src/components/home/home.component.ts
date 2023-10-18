9db0ac7ccbe6cf0025a991312a5ec37d090e169e
Nourhan hesham <nourhanhesham263@gmail.com>
2023-10-18 14:35:48 +0300

    neglecting excessive comments

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,68 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { MovieService } from '../../services/themovieAPI.service';
-// import { Movie } from '../../interface/movie.interface';
-
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.css'],
-// })
-// export class HomeComponent {
-//   movies: Movie[] = [];
-//   searchedvalue!: any;
-//   Movie!: any;
-//   moveees!: any;
-
-//   currentPage: number = 1; // Current page
-//   totalPages: number = 1; // Total pages
-
-//   constructor(private movieService: MovieService) {}
-
-//   ngOnInit(): void {
-//     this.fetchmovies();
-//   }
-
-//   mychild(val: any) {
-//     this.searchedvalue = val;
-//     if (this.searchedvalue) {
-//       this.getmovie(this.searchedvalue);
-//     } else this.fetchmovies();
-//   }
-
-//   getmovie(searchedvalue: string) {
-//     this.movieService.getmovie(searchedvalue).subscribe((data: any) => {
-//       this.movies = data.results;
-//       this.movies.forEach((movie) => {
-//         if (movie.poster_path !== null) {
-//           movie.img = `https://image.tmdb.org/t/p/w370_and_h556_bestv2/${movie.poster_path}`;
-//         } else {
-//           movie.img = 'https://placehold.co/370x566';
-//         }
-//       });
-//     });
-//   }
-
-//   fetchmovies() {
-//     this.movieService.getPopularMovies().subscribe((data) => {
-//       this.movies = data.results;
-//       this.movies.forEach((movie) => {
-//         if (movie.poster_path !== null) {
-//           movie.img = `https://image.tmdb.org/t/p/w370_and_h556_bestv2/${movie.poster_path}`;
-//         } else {
-//           movie.img = 'https://placehold.co/370x566';
-//         }
-//       });
-//     });
-//   }
-
-//   onPageChange(page: number) {
-//     this.currentPage = page;
-//     // Reload data when page changes
-//     this.fetchmovies();
-//   }
-  
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../services/themovieAPI.service';
 import { Movie } from '../../interface/movie.interface';
@@ -104,18 +39,20 @@ export class HomeComponent implements OnInit {
   }
 
   searchMovies(searchedvalue: string) {
-    this.movieService.searchMovies(searchedvalue, this.currentPage).subscribe((data) => {
-      this.movies = data.results;
-      this.totalPages = data.total_pages;
-      // ... Rest of your code for setting movie img paths
-      this.movies.forEach((movie) => {
-        if (movie.poster_path !== null) {
-          movie.img = `https://image.tmdb.org/t/p/w370_and_h556_bestv2/${movie.poster_path}`;
-        } else {
-          movie.img = 'https://placehold.co/370x566';
-        }
+    this.movieService
+      .searchMovies(searchedvalue, this.currentPage)
+      .subscribe((data) => {
+        this.movies = data.results;
+        this.totalPages = data.total_pages;
+        // ... Rest of your code for setting movie img paths
+        this.movies.forEach((movie) => {
+          if (movie.poster_path !== null) {
+            movie.img = `https://image.tmdb.org/t/p/w370_and_h556_bestv2/${movie.poster_path}`;
+          } else {
+            movie.img = 'https://placehold.co/370x566';
+          }
+        });
       });
-    });
   }
 
   fetchMovies() {
@@ -123,12 +60,12 @@ export class HomeComponent implements OnInit {
       this.movies = data.results;
       this.totalPages = data.total_pages;
       this.movies.forEach((movie) => {
-                if (movie.poster_path !== null) {
-                  movie.img = `https://image.tmdb.org/t/p/w370_and_h556_bestv2/${movie.poster_path}`;
-                } else {
-                  movie.img = 'https://placehold.co/370x566';
-                }
-              });
+        if (movie.poster_path !== null) {
+          movie.img = `https://image.tmdb.org/t/p/w370_and_h556_bestv2/${movie.poster_path}`;
+        } else {
+          movie.img = 'https://placehold.co/370x566';
+        }
+      });
     });
   }
 }
