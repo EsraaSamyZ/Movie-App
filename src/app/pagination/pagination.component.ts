fa569a579f8a7690a600336b6cfd6d59e3bb8277
EsraaSamyZ <142722769+EsraaSamyZ@users.noreply.github.com>
2023-10-18 04:31:06 +0300

    added pagination to Home

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.css']
+  styleUrls: ['./pagination.component.css'],
 })
 export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+  @Output() pageChange = new EventEmitter<number>();
+  @Input() maxPages: number = 5;
 
+  get pages(): number[] {
+    const numPages = Math.min(this.totalPages, this.maxPages);
+    return Array.from({ length: numPages }, (_, i) => i + 1);
+  }
+
+  goToPage(page: number): void {
+    if (page >= 1 && page <= this.totalPages && page !== this.currentPage) {
+      this.currentPage = page;
+      this.pageChange.emit(this.currentPage);
+    }
+  }
 }
--- src/services/themovieAPI.service.ts
+++ src/services/themovieAPI.service.ts
@@ -1,3 +1,38 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class MovieService {
+//   private apiKey = '89331dd238f7d3c8400eda6517901145';
+//   private apiUrl = 'https://api.themoviedb.org/3';
+
+//   constructor(private http: HttpClient) {}
+
+//   getPopularMovies(): Observable<any> {
+//     const url = `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`;
+//     return this.http.get(url);
+//   }
+
+//   // FOR Nourhan detailes page
+//   getMovieDetails(movieId: number): Observable<any> {
+//     const url = `${this.apiUrl}/movie/${movieId}?api_key=${this.apiKey}`;
+//     return this.http.get(url);
+//   }
+
+//   getRecommendedMovies(movieId: number): Observable<any> {
+//     const url = `${this.apiUrl}/movie/${movieId}/recommendations?api_key=${this.apiKey}`;
+//     return this.http.get(url);
+//   }
+//   getmovie(MovieName: string): Observable<any> {
+//     const url = `${this.apiUrl}/search/movie?api_key=${this.apiKey}&query=${MovieName}`;
+//     return this.http.get(url);
+//   }
+  
+// }
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -11,24 +46,39 @@ export class MovieService {
 
   constructor(private http: HttpClient) {}
 
-  getPopularMovies(): Observable<any> {
-    const url = `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`;
+  private buildUrl(endpoint: string, queryParams?: Record<string, string>): string {
+    let url = `${this.apiUrl}${endpoint}?api_key=${this.apiKey}`;
+    
+    if (queryParams) {
+      for (const key in queryParams) {
+        if (queryParams.hasOwnProperty(key)) {
+          url += `&${key}=${queryParams[key]}`;
+        }
+      }
+    }
+    
+    return url;
+  }
+
+  getPopularMovies(page: number = 1): Observable<any> {
+    const queryParams = { page: page.toString() };
+    const url = this.buildUrl('/movie/popular', queryParams);
     return this.http.get(url);
   }
 
-  // FOR Nourhan detailes page
   getMovieDetails(movieId: number): Observable<any> {
-    const url = `${this.apiUrl}/movie/${movieId}?api_key=${this.apiKey}`;
+    const url = this.buildUrl(`/movie/${movieId}`);
     return this.http.get(url);
   }
 
   getRecommendedMovies(movieId: number): Observable<any> {
-    const url = `${this.apiUrl}/movie/${movieId}/recommendations?api_key=${this.apiKey}`;
+    const url = this.buildUrl(`/movie/${movieId}/recommendations`);
     return this.http.get(url);
   }
-  getmovie(MovieName: string): Observable<any> {
-    const url = `${this.apiUrl}/search/movie?api_key=${this.apiKey}&query=${MovieName}`;
+
+  searchMovies(movieName: string, page: number = 1): Observable<any> {
+    const queryParams = { query: movieName, page: page.toString() };
+    const url = this.buildUrl('/search/movie', queryParams);
     return this.http.get(url);
   }
-  
 }
